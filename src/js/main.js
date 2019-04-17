162b5a2d43be4dfac84f3d6b5e639616179c5ec3
Luiz Daniel Lima <luizdml@gmail.com>
2019-04-17 20:43:51 -0300

    reponsiveness for menu, main image and stuff

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,15 @@
-$('#videoModal').on('shown.bs.modal', function(e) {
-  //console.log('modal has appeared');
-});
+$(document).ready(function() {
+  $('.toggle-icon').click(function() {
+    console.log('menu icon clicked!');
+    $('.menu-links-ul').toggleClass('active');
+  });
 
-$('#videoModal').on('hidden.bs.modal', function(e) {
-  // console.log('modal has been closed');
-  $('#videoModal iframe').attr('src', $('#videoModal iframe').attr('src'));
+  $('#videoModal').on('shown.bs.modal', function(e) {
+    //console.log('modal has appeared');
+  });
+
+  $('#videoModal').on('hidden.bs.modal', function(e) {
+    // console.log('modal has been closed');
+    $('#videoModal iframe').attr('src', $('#videoModal iframe').attr('src'));
+  });
 });
